3bc2d310060ce5273b6047ee7c5d75028a4a2c9e
Chuck1sn <chuck1inzl@gmail.com>
2025-05-06 21:30:31 +0800

    VITE_APP_PORT

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,5 +1,16 @@
 /// <reference types="vite/client" />
+interface ViteTypeOptions {
+	// By adding this line, you can make the type of ImportMetaEnv strict
+	// to disallow unknown keys.
+	strictImportMetaEnv: unknown;
+}
+
 interface ImportMetaEnv {
 	readonly VITE_ENABLE_MOCK: "true" | "false";
 	readonly VITE_BACKEND_PORT: string;
+	readonly VITE_APP_PORT: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,16 +4,23 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [vue(), vueDevTools(), tailwindcss()],
-	build: {
-		sourcemap: true,
-	},
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		server: {
+			port: Number(env.VITE_APP_PORT),
 		},
-	},
+		plugins: [vue(), vueDevTools(), tailwindcss()],
+		build: {
+			sourcemap: true,
+		},
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
+		},
+	};
 });
